dc109b107a1c9f08a45df8918ca9ef966eafbd42
hiroyuki12 <hiroyuki12@gmail.com>
2015-09-26 22:31:13 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install homebrew
+which brew > /dev/null
+if [ "$?" -ne 0 ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  # Install serverkit and its dependencies
+  sudo which bundle > /dev/null || sudo gem install bundler
+  sudo bundle install > /dev/null
+
+  # Run installer
+  sudo bundle exec serverkit apply recipe.yml
+
