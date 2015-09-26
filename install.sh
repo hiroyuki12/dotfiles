0052b312fff59ac0de1eee7bafd75249d73fa8be
hiroyuki12 <hiroyuki12@gmail.com>
2015-09-26 22:40:24 +0900

    add Download

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+tempfile=/tmp/dotfiles.zip
+workspace=/tmp/dotfiles
+
+# Download zipped installer
+curl -LSfs -o ${tempfile} https://github.com/r7kamura/dotfiles/archive/master.zip
+
+# Unzip installer into workspace
+unzip -oq ${tempfile} -d ${workspace}
+
+# Move to repository root path
+pushd ${workspace}/dotfiles-master > /dev/null
+
 # Install homebrew
 which brew > /dev/null
 if [ "$?" -ne 0 ]; then
