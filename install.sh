643e09317d2aac77022fe923f57354cad6819306
hiroyuki12 <hiroyuki12@gmail.com>
2018-09-27 00:30:05 +0900

    add yes no message

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 tempfile=/tmp/dotfiles.zip
 workspace=/tmp/dotfiles
 
+while true; do
+read -p 'Now Initial Setting? [Y/n]' Answer
+case $Answer in
+  '' | [Yy]* )
+
 # tapToClickOn
 curl -LSfs -o /tmp/tapToClickOn.scpt https://github.com/hiroyuki12/dotfiles/raw/master/appleScript/tapToClickOn.scpt
 osascript /tmp/tapToClickOn.scpt
@@ -28,6 +33,17 @@ curl -LSfs -o /tmp/threeFingerDrag.scpt https://github.com/hiroyuki12/dotfiles/r
 osascript /tmp/threeFingerDrag.scpt
 rm /tmp/threeFingerDrag.scpt
 
+break;
+;;
+[Nn]* )
+  echo "Skip Initial Setting"
+  break;
+  ;;
+* )
+  echo Please answer YES or NO.
+esac
+done;
+
 # Download zipped installer
 curl -LSfs -o ${tempfile} https://github.com/hiroyuki12/dotfiles/archive/master.zip
 
@@ -43,6 +59,11 @@ if [ "$?" -ne 0 ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+while true; do
+read -p 'Now install rbenv? [Y/n]' Answer
+case $Answer in
+  '' | [Yy]* )
+
 # Install rbenv
 brew install rbenv ruby-build rbenv-gemset rbenv-default-gems
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
@@ -51,12 +72,39 @@ rbenv install 2.5.1
 rbenv rehash
 rbenv global 2.5.1
 
+break;
+;;
+[Nn]* )
+  echo "Skip install rbenv"
+  break;
+  ;;
+* )
+  echo Please answer YES or NO.
+esac
+done;
+
+while true; do
+read -p 'Now install bundler serverkit? exec serverkit? [Y/n]' Answer
+case $Answer in
+  '' | [Yy]* )
+
 # Install bundler,serverkit and its dependencies
 sudo which bundle > /dev/null || sudo gem install bundler
 sudo bundle install > /dev/null
 
 # Run installer
 bundle exec serverkit apply recipe.yml.erb
+
+break;
+;;
+[Nn]* )
+  echo "Skip install bundler exec serverkit"
+  break;
+  ;;
+* )
+  echo Please answer YES or NO.
+esac
+done;
 
 # open Basic2.terminal  # add Terminal Profile
 
