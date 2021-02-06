73003d99d2c60ed41b03eb78549c23c89267d915
hiroyuki12 <hiroyuki12@gmail.com>
2021-02-07 01:08:36 +0900

    rbenv 3.0.0

--- install.sh
+++ install.sh
@@ -89,9 +89,9 @@ case $Answer in
 brew install rbenv ruby-build rbenv-gemset rbenv-default-gems
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile
-rbenv install 2.7.0
+rbenv install 3.0.0
 rbenv rehash
-rbenv global 2.7.0
+rbenv global 3.0.0
 
 break;
 ;;
