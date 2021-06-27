05965f81bc3b6169318682e836872326a2a24d35
hiroyuki12 <hiroyuki12@gmail.com>
2021-06-27 22:13:46 +0900

    Add Dockerfile Node.js.

---
+++ docker/node_js/run.sh
@@ -0,0 +1,7 @@
+echo '/app # npm init'
+echo 'packkage name: (app) node-playground'
+echo '/app # vi index.js'
+echo 'console.log(Hello, World!);'
+echo '/app # node.'
+
+docker-compose run --rm app
